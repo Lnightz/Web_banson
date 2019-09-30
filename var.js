874825fe2cfe5311d3e11eeb1178de7f70f3ca6b
Lnightz <lightzzero@gmail.com>
2019-10-01 02:14:31 +0700

    Upload file

---
+++ var.js
@@ -0,0 +1,192 @@
+const data =
+    [
+        {
+            Id: "1",
+            LinkImageSP: "images/son-gac-mooi.jpg",
+            NameSP: "Son dưỡng dầu gấc - Son M.O.O.I",
+            SaleSP: 30000,
+            money: 60000,
+            Category: "Son M.O.O.I",
+            Category1: "Son M.O.O.I",
+            LuotMua: 0,
+            Tag: ["Son", "Son gấc"],
+            Mota: "Đây là sản phẩm gấu nâu",
+        },
+
+        {
+            Id: "2",
+            LinkImageSP: "images/son-dua-mooi.jpg",
+            NameSP: "Son dưỡng dầu dừa - Son M.O.O.I",
+            SaleSP: 20000,
+            money: 35000,
+            Category: "Son M.O.O.I",
+            LuotMua: 0,
+            Tag: ["Son", "Son dừa"],
+            Mota: "Đây là sản phẩm gấu nâu",
+        },
+
+        {
+            Id: "3",
+            LinkImageSP: "images/son-den-mooi.jpg",
+            NameSP: "Son dưỡng củ dền - Son M.O.O.I",
+            SaleSP: 25000,
+            money: 40000,
+            Category: "Son M.O.O.I",
+            LuotMua: 0,
+            Tag: ["Son", "Son dền"],
+            Mota: "Đây là sản phẩm gấu nâu",
+        },
+
+        {
+            Id: "4",
+            LinkImageSP: "images/son-dua-eveline.jpg",
+            NameSP: "Son dưỡng dừa - Eveline",
+            SaleSP: 90000,
+            money: 100000,
+            Category: "Son các hãng khác",
+            LuotMua: 0,
+            Tag: ["son", "son dừa"],
+            Mota: "Son dưỡng dừa của hãng Eveline",
+        },
+
+        {
+            Id: "5",
+            LinkImageSP: "images/son-dua-bambi.jpg",
+            NameSP: "Son dưỡng dừa - Bambi",
+            SaleSP: 60000,
+            money: 65000,
+            Category: "Son các hãng khác",
+            LuotMua: 0,
+            Tag: ["son", "son dừa"],
+            Mota: "Son dưỡng dừa của hãng Bambi",
+        },
+
+        {
+            Id: "6",
+            LinkImageSP: "images/son-dau-gao.jpg",
+            NameSP: "Son dưỡng gạo hương dâu - Cỏ mềm",
+            SaleSP: 80000,
+            money: 90000,
+            Category: "Son các hãng khác",
+            LuotMua: 0,
+            Tag: ["Son", "son gạo"],
+            Mota: "Sản phẩm son gạo hương dâu của hãng cỏ mềm",
+        },
+
+        {
+            Id: "7",
+            LinkImageSP: "images/son-cam-gao.jpg",
+            NameSP: "Son dưỡng gạo hương cam - Cỏ mềm",
+            SaleSP: 80000,
+            money: 90000,
+            Category: "Son các hãng khác",
+            LuotMua: 0,
+            Tag: ["son", "son gạo"],
+            Mota: "Sản phẩm son gạo hương cam của hãng cỏ mềm",
+        },
+
+        {
+            Id: "8",
+            LinkImageSP: "images/son-tra-gao.jpg",
+            NameSP: "Son dưỡng gạo trà xanh - Cỏ mềm",
+            SaleSP: 80000,
+            money: 90000,
+            Category: "Son các hãng khác",
+            LuotMua: 0,
+            Tag: ["son", "son gạo"],
+            Mota: "Sản phẩm son gạo hương trà xanh của hãng cỏ mềm",
+        },
+
+        {
+            Id: "9",
+            LinkImageSP: "images/son-gao.jpg",
+            NameSP: "Son dưỡng gạo - Cỏ mềm",
+            SaleSP: 80000,
+            money: 90000,
+            Category: "Son các hãng khác",
+            LuotMua: 0,
+            Tag: ["son", "son gạo"],
+            Mota: "Sản phẩm son gạo hương gạo của hãng cỏ mềm",
+        },
+
+        {
+            Id: "10",
+            LinkImageSP: "images/son-sap-ong-coco.jpg",
+            NameSP: "Son Dưỡng Ẩm Dầu Dừa Sáp Ong - Coco Secret",
+            SaleSP: 35000,
+            money: "",
+            Category: "Son các hãng khác",
+            LuotMua: 0,
+            Tag: ["Son", "Son dừa"],
+            Mota: "Son Môi Dưỡng Ẩm Dầu Dừa Sáp Ong Coco Secret",
+        },
+
+        {
+            Id: "11",
+            LinkImageSP: "images/son-nuoc-bacha-greengarden.jpg",
+            NameSP: "Son nước tinh dầu bạc hà - Green Garden",
+            SaleSP: 40000,
+            money: "",
+            Category: "Son các hãng khác",
+            LuotMua: 0,
+            Tag: ["Son", "Son nước"],
+            Mota: "Son nước tinh dầu bạc hà của Green Garden",
+        },
+
+        {
+            Id: "12",
+            LinkImageSP: "images/son-nuoc-lavender-greengarden.jpg",
+            NameSP: "Son nước tinh dầu Lavender - Green Garden",
+            SaleSP: 40000,
+            money: "",
+            Category: "Son các hãng khác",
+            LuotMua: 0,
+            Tag: ["Son", "Son nước"],
+            Mota: "Son nước tinh dầu Lavender của Green Garden",
+        },
+
+
+        {
+            Id: "13",
+            LinkImageSP: "images/gac-me-chuot.jpg",
+            NameSP: "Son dưỡng gấc - mẹ chuột",
+            SaleSP: 120000,
+            money: 150000,
+            Category: "Son các hãng khác",
+            LuotMua: 0,
+            Tag: ["Son", "Son Gấc"],
+            Mota:"<p>Sản phẩm <b>gấu bông nhỏ</b> được may với chất liệu vải tốt</p> <p><strong>Thành phần:</strong></p>Bông gòn Vải tơ tằm",
+        },
+        {
+            Id: "14",
+            LinkImageSP: "images/son-gac-bambi.jpg",
+            NameSP: "Son dưỡng gấc - Bambi",
+            SaleSP: 65000,
+            money: 70000,
+            Category: "Son các hãng khác",
+            LuotMua: 0,
+            Tag: ["son", "son gấc"],
+            Mota: "Đây là sản phẩm son dưỡng gấc của hãng Bambi",
+        },
+
+        {
+            Id: "15",
+            LinkImageSP: "images/son-dua-yves.jpg",
+            NameSP: "Son dưỡng dừa - Yves",
+            SaleSP: 100000,
+            money: 110000,
+            Category: "Son các hãng khác",
+            LuotMua: 0,
+            Tag: ["son", "son dừa"],
+            Mota: "Đây là son dưỡng dừa hiệu Yves",
+        },
+
+    ];
+
+function getItemById(id) {
+    console.log('ID: ' + typeof (id) + ' - ' + id);
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].Id == id) return data[i];
+    }
+    // return data[id];;
+}
